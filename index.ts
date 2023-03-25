a29811c69daeb3039ec0ff9627ae89c801e73c14
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2023-03-24 23:37:57 -0600

    feat: add typescript

--- index.ts
+++ index.ts
@@ -1,13 +1,15 @@
+import { Request, Response, Express } from "express";
+
 const express = require("express");
 const dotenv = require("dotenv");
 
 dotenv.config();
 
-const app = express();
+const app: Express = express();
 const port = process.env.PORT;
 
-app.get("/", (req, res) => {
-  res.send("Express + TypeScript Server");
+app.get("/", (req: Request, res: Response) => {
+  res.send("Server is running.");
 });
 
 app.listen(port, () => {
